6d040a359028b11383308940538679a3443e42bf
Dragneol <dragneol@outlook.com.vn>
2018-05-30 17:26:22 +0700

    testing search music

---
+++ Controllers/PlayMusicController.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Http; 
+using Microsoft.AspNetCore.Mvc; 
+using moody.Models; 
+using Newtonsoft.Json; 
+
+namespace moody.Controllers {
+    [Produces("application/json")]
+    [Route("api/PlayMusic")]
+    public class PlayMusicController:Controller {
+        public IEnumerable<Song> getSong(MoodyContext db, string searchField) {
+            List < Song > songs = new List < Song > (); 
+            songs.Add(db.Song
+                .Where(s => s.Title == searchField)
+                // .Where(s => s.Subtitle == searchField)
+                .FirstOrDefault()
+                );
+            return songs;
+
+        }
+    }
+}
