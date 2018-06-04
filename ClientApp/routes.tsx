97e7dd2b2475e7adb53b2079c48a64de981e8c73
nguyenhongphat0 <hongphat.js@gmail.com>
2018-06-04 16:30:37 +0700

    fix confilct

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -6,12 +6,9 @@ import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { MoodyHomeBody } from './components/home/MoodyHomeBody';
 import { UserDashboard } from './components/user/UserDashboard';
-<<<<<<< HEAD
 import {SongsList} from './components/Songs/SongsList';
-=======
 import { AdminDashboard } from './components/admin/AdminDashboard';
 import { AdminLogin } from './components/admin/AdminLogin';
->>>>>>> 9a9b6ac8246392594094438ed713ae58e799a20d
 
 export const routes = (
     <Layout>
@@ -20,11 +17,8 @@ export const routes = (
         <Route path='/fetchdata' component={ FetchData } />
         <Route path='/home' component={ Home }/>
         <Route path='/user' component={ UserDashboard }/>
-<<<<<<< HEAD
         <Route path='/songs' component={ SongsList }/>
-=======
         <Route exact path='/admin' component={ AdminDashboard }/>
         <Route path='/admin/login' component={ AdminLogin }/>
->>>>>>> 9a9b6ac8246392594094438ed713ae58e799a20d
     </Layout>
 );
