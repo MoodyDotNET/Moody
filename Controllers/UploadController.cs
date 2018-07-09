814bd2b71bd029df9b1f4cdb2ba40ac283559b0b
Dragneol <dragneol@outlook.com.vn>
2018-07-09 20:56:23 +0700

    fix Phat's Bugs

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -1,55 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using moody.Models;
-using Newtonsoft.Json;
-using System.IO;
-using System.Web;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Net.Http.Headers;
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Http; 
+using Microsoft.AspNetCore.Mvc; 
+using moody.Models; 
+using Newtonsoft.Json; 
+using System.IO; 
+using System.Web; 
+using Microsoft.AspNetCore.Hosting; 
+using Microsoft.Net.Http.Headers; 
 
-namespace moody.Controllers
-{
+namespace moody.Controllers {
     [Route("api/[controller]")]
-    public class UploadController : Controller
-    {
-        private readonly IHostingEnvironment Environment;
+    public class UploadController:Controller {
+        private readonly IHostingEnvironment Environment; 
 
-        public UploadController(IHostingEnvironment environment)
-        {
-            Environment = environment;
+        public UploadController(IHostingEnvironment environment) {
+            Environment = environment; 
         }
 
         [HttpPut]
-        public string Index(string filename)
-        {
-            if (HttpContext.Request.Form.Files != null)
-            {
-                var files = HttpContext.Request.Form.Files;
-                foreach (var file in files)
-                {
-                    if (file.Length > 0)
-                    {
-                        try
-                        {
-                            filename = Environment.WebRootPath + $@"/{filename}";
-                            using (FileStream fs = System.IO.File.Create(filename))
-                            {
-                                file.CopyTo(fs);
-                                fs.Flush();
+        public string Index(string filename) {
+            if (HttpContext.Request.Form.Files != null) {
+                var files = HttpContext.Request.Form.Files; 
+                foreach (var file in files) {
+                    if (file.Length > 0) {
+                        // try
+                        // {
+                            filename = Environment.WebRootPath + $@"/{filename}"; 
+                            using (FileStream fs = System.IO.File.Create(filename)) {
+                                file.CopyTo(fs); 
+                                fs.Flush(); 
                             }
-                        }
-                        catch
-                        {
-                            throw new Exception("File Too Large");
-                        }
+                        // }
+                        // catch
+                        // {
+                        //     throw new Exception("File Too Large");
+                        // }
                     }
                 }
             }
-            return filename;
+            return filename; 
         }
     }
 }
